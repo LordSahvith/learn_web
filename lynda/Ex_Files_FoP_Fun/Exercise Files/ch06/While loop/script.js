1ab6b6bc1196acd5017f6844637bdb7d02ed5ddd
Caleb <bulletproofchild@gmail.com>
2018-11-10 21:16:48 -0700

    finish chapter 6 : loops

--- lynda/Ex_Files_FoP_Fun/Exercise Files/ch06/While loop/script.js
+++ lynda/Ex_Files_FoP_Fun/Exercise Files/ch06/While loop/script.js
@@ -1,1 +1,21 @@
+var list = 5;
 
+while (list > 0) {
+  console.log(list);
+  list--;
+}
+
+while (list < 5) {
+  console.log(list);
+  list++;
+}
+
+var items = ["apple", "orange", "peach", "banana"];
+
+for (i = 0; i < items.length; i++) {
+  console.log(items[i]);
+}
+
+items.forEach(function(item) {
+  console.log(item);
+});
