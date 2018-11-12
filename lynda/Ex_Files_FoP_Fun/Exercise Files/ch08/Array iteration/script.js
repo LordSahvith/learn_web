4e01d439fd01530e167479e6a349c39c0e8d1234
Caleb <bulletproofchild@gmail.com>
2018-11-11 19:40:43 -0700

    colletions

--- lynda/Ex_Files_FoP_Fun/Exercise Files/ch08/Array iteration/script.js
+++ lynda/Ex_Files_FoP_Fun/Exercise Files/ch08/Array iteration/script.js
@@ -1,12 +1,27 @@
-var myArray = [500,500,500,500,500];
+var myArray = [500, 500, 500, 500, 500];
 
 var total = 0;
 
-for ( var i = 0 ; i < myArray.length ; i++ ) {
-    // add the current element to the total	
-    total = total + myArray[i];
+for (var i = 0; i < myArray.length; i++) {
+  // add the current element to the total	
+  total = total + myArray[i];
 }
 
 // after we're done with the loop
-alert("The total is: " + total);
+printValue("The total is: " + total);
 
+var items = ["apple", "orange", "peach", "banana"];
+
+for (i = 0; i < items.length; i++) {
+  printValue(items[i]);
+}
+
+items.sort();
+
+items.forEach(function (item) {
+  printValue(item);
+});
+
+function printValue(value) {
+  console.log(value);
+}
